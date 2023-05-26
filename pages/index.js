839bcf0c427a0d7a08bcd3799bf20f6b3ae3b1a3
Bilal <bks1733@gmail.com>
2023-05-26 18:03:36 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const { Fragment } = require('react');
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <h1>This is Home Page.</h1>
+    </Fragment>
+  );
+};
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+const NewsPage = () => {
+  return (
+    <Fragment>
+      <h1>This is News Page.</h1>
+    </Fragment>
+  );
+};
+
+export default NewsPage;
