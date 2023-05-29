e778029c2c0df357232d5a8b649ffb81301e1c08
Bilal <bks1733@gmail.com>
2023-05-29 15:47:24 +0530

    Extracting Dynamic Parameter Values.

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,8 +1,15 @@
 // our-domain.com/news/something-important
+import { useRouter } from 'next/router';
 
 import { Fragment } from 'react';
 
 const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send a request to the backend API
+  // to fecth the news item with newsId
   return (
     <Fragment>
       <h1>This is Detail Page.</h1>
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,11 +1,19 @@
 // our-domain.com/news
-
+import Link from 'next/link';
 import { Fragment } from 'react';
 
 const NewsPage = () => {
   return (
     <Fragment>
       <h1>This is News Page.</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
     </Fragment>
   );
 };
